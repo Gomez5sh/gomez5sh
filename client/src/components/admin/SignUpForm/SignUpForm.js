5b2742839349ef9d27849116a51fb39ed732294c
g5sh <eagomez2387@gmail.com>
2020-07-13 17:32:03 -0500

    [ADD] CLIENT: Add Sign up form an scss

--- client/src/components/admin/SignUpForm/SignUpForm.js
+++ client/src/components/admin/SignUpForm/SignUpForm.js
@@ -6,44 +6,46 @@ import "./SignUpForm.scss";
 
 export default function SignUpForm() {
   return (
-    <Form className="register-form">
-      <Form.Item>
-        <Input
-          prefix={<Icon type="user" style={{ color: "rgba:0,0,0,.25" }} />}
-          type="email"
-          name="email"
-          placeholder="E-mail"
-          className="register-form__input"
-        />
-      </Form.Item>
-      <Form.Item>
-        <Input
-          prefix={<Icon type="lock" style={{ color: "rgba:0,0,0,.25" }} />}
-          type="password"
-          name="password"
-          placeholder="password"
-          className="register-form__input"
-        />
-      </Form.Item>
-      <Form>
-        <Input
-          prefix={<Icon type="lock" style={{ color: "rgba:0,0,0,.25" }} />}
-          type="password"
-          name="repeatPassword"
-          placeholder="Repeat your password"
-          className="register-form__input"
-        />
+    <div>
+      <Form className="register-form">
+        <Form.Item>
+          <Input
+            prefix={<Icon type="user" style={{ color: "rgba:0,0,0,.25" }} />}
+            type="email"
+            name="email"
+            placeholder="E-mail"
+            className="register-form__input"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Input
+            prefix={<Icon type="lock" style={{ color: "rgba:0,0,0,.25" }} />}
+            type="password"
+            name="password"
+            placeholder="password"
+            className="register-form__input"
+          />
+        </Form.Item>
+        <Form>
+          <Input
+            prefix={<Icon type="lock" style={{ color: "rgba:0,0,0,.25" }} />}
+            type="password"
+            name="repeatPassword"
+            placeholder="Repeat your password"
+            className="register-form__input"
+          />
+        </Form>
+        <Form.Item>
+          <Checkbox name="privacyPolicy">
+            I have read and accept the privacy policy
+          </Checkbox>
+        </Form.Item>
+        <Form.Item>
+          <Button htmlType="submit" className="register-form__button">
+            Create Account
+          </Button>
+        </Form.Item>
       </Form>
-      <Form.Item>
-        <Checkbox name="privacyPolicy">
-          I have read and accept the privacy policy
-        </Checkbox>
-      </Form.Item>
-      <Form.Item>
-        <Button htmlType="submit" className="register-form__button">
-          Create Account
-        </Button>
-      </Form.Item>
-    </Form>
+    </div>
   );
 }
